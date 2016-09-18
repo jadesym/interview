ad5012b877ae3d875237044b600a3b4f98b5a0e3
Kevin Fu <kevin.fu808@gmail.com>
2016-09-18 05:00:55 -0700

    added some manual testing for search tree mod

--- search_tree_mod/search_tree_mod.py
+++ search_tree_mod/search_tree_mod.py
@@ -8,8 +8,10 @@ class Node:
         self.mid = mid
 
 class SearchTreeMod:
-    def __init__(self, root = None):
+    def __init__(self, values, root = None):
         self.root = root
+        for value in values:
+            self.insert(value)
     # Inserts a new Node into the tree
     def insert(self, value):
         if self.root is None:
@@ -33,42 +35,48 @@ class SearchTreeMod:
                 node.right = Node(value)
             else:
                 self.insert_recurse(node.right, value)
+    def delete_multiple(self, values):
+        for value in values:
+            self.delete(value)
     # Deletes a Node from the tree by its value
     def delete(self, value):
         if self.root is None:
             return
         else:
-            self.delete_recurse(self.root, value, None)
+            self.delete_recurse(self.root, value, None, None)
     # Deletes a Node from a subtree by its value
-    def delete_recurse(self, node, value):
+    def delete_recurse(self, node, value, parent, parentleftOrRight):
         if node.val == value:
-            if node.mid is not None:
-                self.delete_lowest_mid(node)
-            else:
-                # replace Call
+            if node.mid is not None: self.delete_lowest_mid(node)
+            else: self.replace(node, parent, parentleftOrRight)
         elif node.val > value:
-            if node.left is not None:
-                self.delete_recurse(node.left, value, parent)
+            if node.left is not None: self.delete_recurse(node.left, value, node, True)
         else:
-            if node.right is not None:
-                self.delete_recurse(node.right, value, parent)
+            if node.right is not None: self.delete_recurse(node.right, value, node, False)
     # Deletes the lowest mid node that exists
     def delete_lowest_mid(self, node):
-        if node.mid.mid is None:
-            node.mid is None
-        else:
-            self.delete_lowest_mid(node.mid)
+        if node.mid.mid is None: node.mid = None
+        else: self.delete_lowest_mid(node.mid)
     # Finds the replacement node
-    def replace(self, node, parent):
+    def replace(self, node, parent, parentLeftOrRight):
         left = self.find_replacement(True, node.left, parent)
         if left is not None:
-            node.val = left.val
+            if parent is not None:
+                if parentLeftOrRight: parent.left = left
+                else: parent.right = left
+            else: self.root = left
+            left.right = node.right
             return
         right = self.find_replacement(False, node.right, parent)
-        if right is not None: node.val = right.val
+        if right is not None:
+            if parent is not None:
+                if parentLeftOrRight: parent.left = right
+                else: parent.right = right
+            else: self.root = right
+            right.left = node.left
         else:
             if parent is None:
-                self.root is None:
+                self.root = None
             else:
                 if parent.left is node: parent.left = None
                 elif parent.right is node: parent.right = None
@@ -81,22 +89,45 @@ class SearchTreeMod:
             while curNode.right is not None:
                 prevNode = curNode
                 curNode = curNode.right
-            prevNode.right = None
+            if prevNode is not None: prevNode.right = None
         else:
             while curNode.left is not None:
                 prevNode = curNode
                 curNode = curNode.left
-            prevNode.left = None
+            if prevNode is not None: prevNode.left = None
         return curNode
     # Printing for Debugging Purposes
     def display(self):
         queue = deque([(0, self.root)])
-        prevLevel = 0
+        prevLevel = -1
+        beginning = True
+        print("Starting SMT")
         while len(queue) > 0:
-            level, node = level, queue.popleft()
+            level, node = queue.popleft()
+            if node is None: continue
             if level > prevLevel:
-                print()
-            print(node.val, end = "")
-            if node.left is not None: queue.append(node.left)
-            if node.mid is not None: queue.append(node.mid)
-            if node.right is not None: queue.append(node.right)
+                if not beginning:
+                    print()
+                else:
+                    beginning = False
+                print(str(level) + ":", end="")
+                prevLevel = level
+            print(str(node.val), ":", end = "")
+            children = []
+            if node.left is not None:
+                queue.append((level + 1, node.left))
+                children.append(node.left.val)
+            else:
+                children.append("_")
+            if node.mid is not None:
+                queue.append((level + 1, node.mid))
+                children.append(node.mid.val)
+            else:
+                children.append("_")
+            if node.right is not None:
+                queue.append((level + 1, node.right))
+                children.append(node.right.val)
+            else:
+                children.append("_")
+            print(str(children) + " ", " ", end = "")
+        print("\nDone...")
